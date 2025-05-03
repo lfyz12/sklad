bae16e72f6361de2e95854fe0188881d61ae0568
Danil <kraskafet@gmail.com>
2025-05-03 20:59:37 +0500

    back fix 4

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const errorMiddleware = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000
 
 const corsOptions ={
-    origin:'http://localhost:3000',
+    origin: process.env.CLIENT_URL,
     credentials:true,
     optionSuccessStatus:200
 }
