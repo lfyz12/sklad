14fc86a86ea959a7cd2496434dff3a3b677a191a
xxxioan <kraskafet@gmail.com>
2025-05-04 23:09:55 +0500

    Update ProductController.js

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -47,7 +47,7 @@ class ProductController {
     // Обновление информации о соискателе
     async update(req, res, next) {
         try {
-            const { Id, Name, Price, Quantity Description } = req.body;
+            const { Id, Name, Price, Quantity, Description } = req.body;
 
             const updated = await Product.update(
                 {
